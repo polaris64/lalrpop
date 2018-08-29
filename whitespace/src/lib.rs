14cb3db504cb333debd1e2b76fb113285ff430c6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-29 17:16:08 +0000

    Deploy lalrpop/lalrpop to github.com/lalrpop/lalrpop.git:gh-pages

---
+++ whitespace/src/lib.rs
@@ -0,0 +1,24 @@
+#[macro_use] extern crate lalrpop_util;
+
+pub mod lexer;
+pub mod ast;
+pub mod eval;
+
+lalrpop_mod!(pub parser);
+
+pub fn compile(input: &str) -> Result<ast::Program, String> {
+    match parser::ProgramParser::new().parse(lexer::Lexer::new(input)) {
+        Ok(s) => Ok(ast::Program::new(s)),
+        Err(e) => Err(format!("{:?}", e)),
+    }
+}
+
+#[test]
+fn parse_simple() {
+    let input = lexer::Lexer::new("\n\n\n");
+    let program = parser::ProgramParser::new().parse(input).expect("Oh no");
+    match (program.len(), program.first()) {
+        (1, Some(&ast::Stmt::Exit)) => (),
+        other => panic!("Well that didn't work: {:?}", other),
+    }
+}
